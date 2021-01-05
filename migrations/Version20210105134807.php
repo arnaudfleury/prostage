3fe6bd1c3d7db34e41b8d97b93856ea3d50ff032
Fleury Arnaud <afleury005@iutbayonne.univ-pau.fr>
2021-01-05 14:48:55 +0100

    Migration effectuée

---
+++ migrations/Version20210105134807.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210105134807 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE entreprise (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) DEFAULT NULL, adresse VARCHAR(300) DEFAULT NULL, site_web VARCHAR(150) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, sigle VARCHAR(30) DEFAULT NULL, intitule VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stage (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(100) DEFAULT NULL, date DATE DEFAULT NULL, activite VARCHAR(50) DEFAULT NULL, lieu VARCHAR(300) DEFAULT NULL, description VARCHAR(3000) DEFAULT NULL, archive TINYINT(1) NOT NULL, mail VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE entreprise');
+        $this->addSql('DROP TABLE formation');
+        $this->addSql('DROP TABLE stage');
+    }
+}
