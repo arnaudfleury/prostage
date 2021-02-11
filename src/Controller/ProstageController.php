f60deed7bf2c8618079c57c24c0c97f95906676f
Fleury Arnaud <afleury005@iutbayonne.univ-pau.fr>
2021-02-11 10:38:08 +0100

    Requetes personnalisées

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -20,66 +20,6 @@ class ProstageController extends AbstractController
     }
 
     /**
-     * @Route("/entreprises", name="prostage_entreprises")
-     */
-    public function entreprises(): Response
-    {
-        // Récupérer le repository de l'entité Entreprise
-        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
-
-        // Récupérer les entreprises enregistrées en BD
-        $entreprises = $repositoryEntreprise->findAll();
-
-          // Envoyer les entreprises récupérées à la vue chargée de les afficher
-        return $this->render('prostage/entreprises.html.twig',['entreprises'=>$entreprises]);
-    }
-
-    /**
-     * @Route("/entreprise/{id}", name="prostage_entreprise")
-     */
-    public function entreprise($id): Response
-    {
-        // Récupérer le repository de l'entité Entreprise
-        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
-
-        // Récupérer l'entreprise enregistrée en BD
-        $entreprise = $repositoryEntreprise->find($id);
-
-        // Envoyer l'entreprise récupérée à la vue chargée de l'afficher
-        return $this->render('prostage/entreprise.html.twig',['entreprise' => $entreprise]);
-    }
-
-    /**
-     * @Route("/formations", name="prostage_formations")
-     */
-    public function formations(): Response
-    {
-        // Récupérer le repository de l'entité Formation
-        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
-
-        // Récupérer les formations enregistrées en BD
-        $formations = $repositoryFormation->findAll();
-
-        // Envoyer les formations récupérées à la vue chargée de les afficher
-        return $this->render('prostage/formations.html.twig',['formations' => $formations]);
-    }
-
-    /**
-     * @Route("/formation/{id}", name="prostage_formation")
-     */
-    public function formation($id): Response
-    {
-        // Récupérer le repository de l'entité Formation
-        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
-
-        // Récupérer la formation enregistrée en BD
-        $formation = $repositoryFormation->find($id);
-
-        // Envoyer la formation récupérée à la vue chargée de l'afficher
-        return $this->render('prostage/formation.html.twig',['formation' => $formation]);
-    }
-
-    /**
      * @Route("/stages", name="prostage_stages")
      */
     public function stages(): Response
@@ -88,7 +28,7 @@ class ProstageController extends AbstractController
         $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
 
         // Récupérer les stages enregistrés en BD
-        $stages = $repositoryStage->findAll();
+        $stages = $repositoryStage->findAllJoinEntrepriseAndFormation();
 
         // Envoyer les stages récupérés à la vue chargée de les afficher
         return $this->render('prostage/stages.html.twig',['stages'=>$stages]);
@@ -107,6 +47,78 @@ class ProstageController extends AbstractController
 
         // Envoyer le stage récupéré à la vue chargée de l'afficher
         return $this->render('prostage/stage.html.twig',['stage' => $stage]);
+    }
+
+    /**
+     * @Route("/entreprises", name="prostage_entreprises")
+     */
+    public function entreprises(): Response
+    {
+        // Récupérer le repository de l'entité Entreprise
+        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+
+        // Récupérer les entreprises enregistrées en BD
+        $entreprises = $repositoryEntreprise->findAll();
+
+          // Envoyer les entreprises récupérées à la vue chargée de les afficher
+        return $this->render('prostage/entreprises.html.twig',['entreprises'=>$entreprises]);
+    }
+
+    /**
+     * @Route("/entreprise/{nomEntreprise}", name="prostage_entreprise")
+     */
+    public function entreprise($nomEntreprise): Response
+    {
+        // Récupérer le repository de l'entité Entreprise
+        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+
+        // Récupérer l'entreprise enregistrée en BD
+        $entreprise = $repositoryEntreprise->findOneByNom($nomEntreprise);
+
+        // Récupérer le repository de l'entité Stage
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+
+        // Récupérer les stages enregistrés en BD
+        $stages = $repositoryStage->findByNomEntreprise($nomEntreprise);
+
+        // Envoyer l'entreprise récupérée à la vue chargée de l'afficher
+        return $this->render('prostage/entreprise.html.twig',['entreprise' => $entreprise,'stages' => $stages]);
+    }
+
+    /**
+     * @Route("/formations", name="prostage_formations")
+     */
+    public function formations(): Response
+    {
+        // Récupérer le repository de l'entité Formation
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+
+        // Récupérer les formations enregistrées en BD
+        $formations = $repositoryFormation->findAll();
+
+        // Envoyer les formations récupérées à la vue chargée de les afficher
+        return $this->render('prostage/formations.html.twig',['formations' => $formations]);
+    }
+
+    /**
+     * @Route("/formation/{sigleFormation}", name="prostage_formation")
+     */
+    public function formation($sigleFormation): Response
+    {
+      // Récupérer le repository de l'entité Formation
+      $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+
+      // Récupérer la formation enregistrée en BD
+      $formation = $repositoryFormation->findOneBySigle($sigleFormation);
+
+      // Récupérer le repository de l'entité Stage
+      $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+
+      // Récupérer les stages enregistrés en BD
+      $stages = $repositoryStage->findBySigleFormation($sigleFormation);
+
+      // Envoyer la formation récupérée à la vue chargée de l'afficher
+      return $this->render('prostage/formation.html.twig',['formation' => $formation,'stages' => $stages]);
     }
 
 
--- src/Repository/StageRepository.php
+++ src/Repository/StageRepository.php
@@ -19,22 +19,55 @@ class StageRepository extends ServiceEntityRepository
         parent::__construct($registry, Stage::class);
     }
 
-    // /**
-    //  * @return Stage[] Returns an array of Stage objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+    * @return Stage[] Returns an array of Stage objects
+    */
+
+    public function findAllJoinEntrepriseAndFormation()
     {
         return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
+            ->addSelect('e, f')
+            ->join('s.entreprise','e')
+            ->join('s.formations','f')
+            ->orderBy('s.date', 'DESC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+
+    public function findByNomEntreprise($nomEntreprise)
+    {
+        return $this->createQueryBuilder('s')
+            ->join('s.entreprise','e')
+            ->andWhere('e.nom = :nomEntreprise')
+            ->setParameter('nomEntreprise', $nomEntreprise)
+            ->orderBy('s.date', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findBySigleFormation($sigleFormation)
+    {
+       // Récupérer le gestionnaire d'entité
+       $entityManager = $this->getEntityManager();
+
+       // Construction de la requête
+       $requete = $entityManager->createQuery(
+         'SELECT s, f
+          FROM App\Entity\Stage s
+          JOIN App\Entity\Formation f
+          WHERE s.id = f.id
+          AND f.sigle = :sigleFormation'
+       );
+
+       // Définition de la valeur du paramètre injecté dans la requête
+       $requete->setParameter('sigleFormation',$sigleFormation);
+
+       // Exécuter la requête et retourner les résultats
+       return $requete->execute();
+    }
+
 
     /*
     public function findOneBySomeField($value): ?Stage
